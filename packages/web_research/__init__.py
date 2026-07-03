1ad05c9183f54e8615931c7910bc380787dcdd79
Gucci <57073159+GucciGross@users.noreply.github.com>
2026-07-02 22:00:16 -0500

    Export Firecrawl local client

--- packages/web_research/__init__.py
+++ packages/web_research/__init__.py
@@ -1,3 +1,4 @@
+from .firecrawl_local import FirecrawlLocalClient
 from .searxng import SearxngSearchClient, SearchResult
 
-__all__ = ["SearxngSearchClient", "SearchResult"]
+__all__ = ["FirecrawlLocalClient", "SearxngSearchClient", "SearchResult"]
